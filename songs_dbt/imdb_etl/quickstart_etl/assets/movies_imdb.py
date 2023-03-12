113cf1d4f8a04e9b138d10dcddc79bc5dbc66144
Anton <anton.peniaziev@explorium.ai>
2023-03-12 09:44:19 +0200

    dagster

--- songs_dbt/imdb_etl/quickstart_etl/assets/movies_imdb.py
+++ songs_dbt/imdb_etl/quickstart_etl/assets/movies_imdb.py
@@ -1,4 +1,7 @@
-from dagster import build_init_resource_context, io_manager, with_resources, file_relative_path
+import json
+
+from dagster import build_init_resource_context, io_manager, with_resources, file_relative_path, \
+    load_assets_from_modules, Definitions
 from dagster_aws.s3 import s3_pickle_io_manager
 import base64
 from io import BytesIO
@@ -15,14 +18,13 @@ from dagster._core.definitions.cacheable_assets import CacheableAssetsDefinition
 from dagster_airbyte import load_assets_from_airbyte_instance
 from quickstart_etl.external_api.imdb import ImdbApi
 from dagster import AssetIn, MetadataValue, AssetOut
-from dagster_dbt import load_assets_from_dbt_project, dbt_cli_resource
-
+from dagster_dbt import load_assets_from_dbt_project, dbt_cli_resource, load_assets_from_dbt_manifest
 
 DBT_PROJECT_PATH = file_relative_path(__file__, "../imdb_dbt")
 DBT_PROFILES = file_relative_path(__file__,     "../imdb_dbt/config")
 
 # if larger project use load_assets_from_dbt_manifest
-# dbt_assets = load_assets_from_dbt_manifest(json.load(DBT_PROJECT_PATH + "manifest.json", encoding="utf8"))
+
 
 
 @asset(group_name="imbd", compute_kind="IMDB API", key_prefix=["imdb_api"])
@@ -43,102 +45,50 @@ my_airbyte_resource=airbyte_resource.configured(
     }
     )
 
-ab_assets = with_resources(
+ab_assets_movies = with_resources(
     build_airbyte_assets(
         connection_id="13fb7a92-dbf0-44ad-a5f5-2f4286ac84d0",
         destination_tables=["movies"],
-        asset_key_prefix=["movies"]
+        asset_key_prefix=["company_db"]
     ),
     {"airbyte": my_airbyte_resource},
 )
 
-dbt_assets = load_assets_from_dbt_project(
-    project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["imdb_dbt"], source_key_prefix=["company_db"]
+ab_assets_actors = with_resources(
+    build_airbyte_assets(
+        connection_id="b89d861d-eea5-4aa7-a217-98fe6401da7c",
+        destination_tables=["actors"],
+        asset_key_prefix=["company_db"]
+    ),
+    {"airbyte": my_airbyte_resource},
 )
+
+ab_assets_writers = with_resources(
+    build_airbyte_assets(
+        connection_id="e9bb0f62-1ba6-4388-b942-17a94e4eb94b",
+        destination_tables=["writers"],
+        asset_key_prefix=["company_db"]
+    ),
+    {"airbyte": my_airbyte_resource},
+)
+
+dbt_assets = load_assets_from_dbt_manifest(manifest_json=json.load(open(DBT_PROJECT_PATH + "/target/manifest.json")),
+                                           key_prefix=["imdb_dbt"])
+# dbt_assets = load_assets_from_dbt_project(
+#     project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["imdb_dbt"]
+# )
 dbt_assets = with_resources(
     dbt_assets,
     {"dbt": dbt_cli_resource},
 )
 # airbyte_assets = load_assets_from_airbyte_instance(my_airbyte_resource)
 
+# module_assets = load_assets_from_modules([assets])
+# @job(resource_defs={"airbyte": my_airbyte_resource})
+# def my_simple_airbyte_job():
+#     sync_fake()
 
 
+# defs = Definitions(assets=list(module_assets), resources=resources)
 
 
-#
-#
-# @asset(group_name="hackernews", compute_kind="HackerNews API")
-# def hackernews_topstory_ids() -> List[int]:
-#     """
-#     Get up to 500 top stories from the HackerNews topstories endpoint.
-#
-#     API Docs: https://github.com/HackerNews/API#new-top-and-best-stories
-#     """
-#     newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
-#     top_500_newstories = requests.get(newstories_url).json()
-#     return top_500_newstories
-#
-#
-# @asset(group_name="hackernews", compute_kind="HackerNews API")
-# def hackernews_topstories(
-#     context: OpExecutionContext, hackernews_topstory_ids: List[int]
-# ) -> pd.DataFrame:
-#     """
-#     Get items based on story ids from the HackerNews items endpoint. It may take 1-2 minutes to fetch all 500 items.
-#
-#     API Docs: https://github.com/HackerNews/API#items
-#     """
-#     results = []
-#     for item_id in hackernews_topstory_ids:
-#         item = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json").json()
-#         results.append(item)
-#         if len(results) % 20 == 0:
-#             context.log.info(f"Got {len(results)} items so far.")
-#
-#     df = pd.DataFrame(results)
-#
-#     # Dagster supports attaching arbitrary metadata to asset materializations. This metadata will be
-#     # shown in the run logs and also be displayed on the "Activity" tab of the "Asset Details" page in the UI.
-#     # This metadata would be useful for monitoring and maintaining the asset as you iterate.
-#     # Read more about in asset metadata in https://docs.dagster.io/concepts/assets/software-defined-assets#recording-materialization-metadata
-#     context.add_output_metadata(
-#         {
-#             "num_records": len(df),
-#             "preview": MetadataValue.md(df.head().to_markdown()),
-#         }
-#     )
-#     return df
-#
-#
-# @asset(group_name="hackernews", compute_kind="Plot")
-# def hackernews_topstories_word_cloud(
-#     context: OpExecutionContext, hackernews_topstories: pd.DataFrame
-# ) -> bytes:
-#     """
-#     Exploratory analysis: Generate a word cloud from the current top 500 HackerNews top stories.
-#     Embed the plot into a Markdown metadata for quick view.
-#
-#     Read more about how to create word clouds in http://amueller.github.io/word_cloud/.
-#     """
-#     stopwords = set(STOPWORDS)
-#     stopwords.update(["Ask", "Show", "HN"])
-#     titles_text = " ".join([str(item) for item in hackernews_topstories["title"]])
-#     titles_cloud = WordCloud(stopwords=stopwords, background_color="white").generate(titles_text)
-#
-#     # Generate the word cloud image
-#     plt.figure(figsize=(8, 8), facecolor=None)
-#     plt.imshow(titles_cloud, interpolation="bilinear")
-#     plt.axis("off")
-#     plt.tight_layout(pad=0)
-#
-#     # Save the image to a buffer and embed the image into Markdown content for quick view
-#     buffer = BytesIO()
-#     plt.savefig(buffer, format="png")
-#     image_data = base64.b64encode(buffer.getvalue())
-#     md_content = f"![img](data:image/png;base64,{image_data.decode()})"
-#
-#     # Attach the Markdown content as metadata to the asset
-#     # Read about more metadata types in https://docs.dagster.io/_apidocs/ops#metadata-types
-#     context.add_output_metadata({"plot": MetadataValue.md(md_content)})
-#
-#     return image_data
--- songs_dbt/imdb_etl/quickstart_etl/imdb_dbt/models/imdb_analysis/movies_stats.sql
+++ songs_dbt/imdb_etl/quickstart_etl/imdb_dbt/models/imdb_analysis/movies_stats.sql
@@ -1,3 +1,5 @@
 
 
-select * from {{ source('company_db', 'movies') }}
+select * from {{ source('company_db', 'movies') }} m join {{ source('company_db', 'actors') }} a
+         on m.id = a.movie_id join {{ source('company_db', 'writers') }} w on m.id = w.movie_id
+
